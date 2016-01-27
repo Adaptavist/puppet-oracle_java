9427eed43f3d7b9933083bfcd91fd317b5b7926e
Jon Mort <jonmort@gmail.com>
2016-01-27 17:22:27 +0000

    fix linter errors

--- manifests/java_install.pp
+++ manifests/java_install.pp
@@ -1,4 +1,4 @@
-define oracle_java::java_install($available, $alt_names, $ensure = "latest") {
+define oracle_java::java_install($available, $alt_names, $ensure = 'latest') {
     $package_name=$available[$name]
     if has_key($available, $name) {
         if ($name == '6' and $::operatingsystem == 'ubuntu' and $::operatingsystemrelease == '14.04') {
@@ -12,10 +12,10 @@ define oracle_java::java_install($available, $alt_names, $ensure = "latest") {
             }
         }
 
-        if $::osfamily == "RedHat"{
+        if $::osfamily == 'RedHat'{
             $alt_name = $alt_names[$name]
-            $alt_cmd_java="alternatives --install /usr/bin/java java /usr/java/$(ls /usr/java/ | grep jdk1.$name)/bin/java 200000"
-            $alt_cmd_javac="alternatives --install /usr/bin/javac javac /usr/java/$(ls /usr/java/ | grep jdk1.$name)/bin/javac 200000"
+            $alt_cmd_java="alternatives --install /usr/bin/java java /usr/java/$(ls /usr/java/ | grep jdk1.${name})/bin/java 200000"
+            $alt_cmd_javac="alternatives --install /usr/bin/javac javac /usr/java/$(ls /usr/java/ | grep jdk1.${name})/bin/javac 200000"
             exec { "Install alternatives for java ${name}":
                 command  => $alt_cmd_java,
                 provider => shell,
