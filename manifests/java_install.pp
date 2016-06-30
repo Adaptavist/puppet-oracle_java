71d03270a8c7708a4f30afa36bde3ab9aeb98f90
Matthew Hope <mhope@adaptavist.com>
2016-06-30 11:53:51 +0100

    added code to handle multiple minor versions of the same major on RH based systems

--- manifests/java_install.pp
+++ manifests/java_install.pp
@@ -14,8 +14,8 @@ define oracle_java::java_install($available, $alt_names, $ensure = 'latest') {
 
         if $::osfamily == 'RedHat'{
             $alt_name = $alt_names[$name]
-            $alt_cmd_java="alternatives --install /usr/bin/java java /usr/java/$(ls /usr/java/ | grep jdk1.${name})/bin/java 200000"
-            $alt_cmd_javac="alternatives --install /usr/bin/javac javac /usr/java/$(ls /usr/java/ | grep jdk1.${name})/bin/javac 200000"
+            $alt_cmd_java="alternatives --install /usr/bin/java java /usr/java/$(ls /usr/java/ | grep jdk1.${name} | sort | tail -1)/bin/java 200000"
+            $alt_cmd_javac="alternatives --install /usr/bin/javac javac /usr/java/$(ls /usr/java/ | grep jdk1.${name} | sort | tail -1)/bin/javac 200000"
             exec { "Install alternatives for java ${name}":
                 command  => $alt_cmd_java,
                 provider => shell,
@@ -26,7 +26,7 @@ define oracle_java::java_install($available, $alt_names, $ensure = 'latest') {
                 provider => shell,
             }
             exec { "Create symlink for jdk in /var/lib/jvm/jdk${name}":
-                command  => "ln -sf /usr/java/$(ls /usr/java/ | grep jdk1.${name}) /usr/lib/jvm/jdk${name}",
+                command  => "ln -sf /usr/java/$(ls /usr/java/ | grep jdk1.${name} | sort | tail -1) /usr/lib/jvm/jdk${name}",
                 provider => shell,
                 require  => [Package[$package_name], File['/usr/lib/jvm']]
             }
